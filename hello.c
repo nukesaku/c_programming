f1e1a92aff68adac9546edbacc2cd881429752c8
nukesaku <nextniconico@gmail.com>
2023-06-04 14:19:26 +0900

    :+1:add print_number

---
+++ hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("こんにちは\n");
+  printf("今日も良い天気です");
+}
---
+++ print_number.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("今年は西暦%d年です\n", 2022);
+  printf("フルマラソンのコースは%.3fキロ\n", 42.195);
+  printf("%d歳の男の子の平均身長は%.1fcm\n", 10, 138.9);
+}
